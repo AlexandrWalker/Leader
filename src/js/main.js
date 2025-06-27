6b3b8b38dff9b33dd4ecbbd4b4ffe0d203ed92c4
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-06-27 20:35:49 +0700

    add burger & footer

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,63 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   gsap.ticker.lagSmoothing(0);
 
+
+
+  /**
+   * Управляет поведением меню-бургера.
+   */
+  function burgerNav() {
+    const burgerBtn = document.querySelector('.burger-btn');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const burgerClose = document.querySelector('.burger-close');
+    const overlay = document.querySelector('.burger-overlay');
+    const elements = document.querySelectorAll('.burger-menu__list a');
+
+    /**
+     * Переключает видимость меню.
+     */
+    const toggleMenu = () => {
+      const isOpened = burgerBtn.classList.toggle('burger-btn--opened');
+      burgerMenu.classList.toggle('burger-menu--opened', isOpened);
+      lenis.stop();
+    };
+
+    /**
+     * Закрывает меню.
+     */
+    const closeMenu = () => {
+      burgerBtn.classList.remove('burger-btn--opened');
+      burgerMenu.classList.remove('burger-menu--opened');
+      lenis.start();
+    };
+
+    // Открытие/закрытие меню по клику на бургер
+    burgerBtn.addEventListener('click', toggleMenu);
+
+    // Закрытие меню по клику на кнопку закрытия или на overlay
+    [burgerClose, overlay].forEach((element) => element.addEventListener('click', closeMenu));
+
+    // Закрытие меню при клике вне области меню и бургера
+    document.addEventListener('click', (event) => {
+      if (!burgerMenu.contains(event.target) && !burgerBtn.contains(event.target)) {
+        closeMenu();
+      }
+    });
+
+    // Закрытие меню при нажатии на кнопку "Esc"
+    window.addEventListener('keydown', (e) => {
+      if (e.key === "Escape") {
+        closeMenu();
+      }
+    });
+
+    elements.forEach((element) => element.addEventListener('click', closeMenu));
+  }
+  
+  burgerNav();
+
+
+
   document.querySelectorAll('.accordion-parent').forEach((accordionContainer) => {
 
     var accordionHead = accordionContainer.querySelectorAll('.accordion'),
